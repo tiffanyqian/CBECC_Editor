37500192c3d406cefd82dc6c772be1b9ca3a7fc6
Tiffany Qian <tffnyqian@gmail.com>
2024-09-12 14:32:36 -0700

    Temporarily Suspending comparefiles.py

--- comparefiles.py
+++ comparefiles.py
@@ -1,15 +1,18 @@
 from difflib import Differ
 import os.path
 
-if os.path.isfile("differences.txt"):
-    wr_fname = open("differences.txt", "w")
-else:
-    wr_fname = open("differences.txt", "x")
+print("CURRENTLY BROKEN! DO NOT USE.")
 
-with open('NR 3030 Nebraska.cibd22x') as file_1, open('testing.cibd22x') as file_2:
-    differ = Differ()
+# if os.path.isfile("differences.txt"):
+#     wr_fname = open("differences.txt", "w")
+# else:
+#     wr_fname = open("differences.txt", "x")
 
-    for line in differ.compare(file_1.readlines(), file_2.readlines()):
-        wr_fname.write(line)
+# with open('Seawall T3.cibd22x') as file_1, open('testing_output.cibd22x') as file_2:
+#     differ = Differ()
 
-wr_fname.close()
+#     for line in differ.compare(file_1.readlines(), file_2.readlines()):
+#         wr_fname.write(line)
+
+# wr_fname.close()
+# print("Differences Logged!")
