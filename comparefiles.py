8d80c8b01e0921e21babf16e1436548815edfa16
Tiffany Qian <tffnyqian@gmail.com>
2024-09-17 09:02:26 -0700

    comparefiles.py is working again, updated file and Readme

--- comparefiles.py
+++ comparefiles.py
@@ -1,18 +1,22 @@
 from difflib import Differ
-import os.path
+import os
 
-print("CURRENTLY BROKEN! DO NOT USE.")
+## ** USER INPUT ** -------------------
+# File Names to compare:
+f1 = "INPUT_FIRST_FILENAME_HERE.cibd22x"
+f2 = "INPUT_SECOND_FILENAME_HERE.cibd22x"
+## ------------------------------------
 
-# if os.path.isfile("differences.txt"):
-#     wr_fname = open("differences.txt", "w")
-# else:
-#     wr_fname = open("differences.txt", "x")
+if os.path.isfile("differences.txt"):
+    wr_fname = open("differences.txt", "w")
+else:
+    wr_fname = open("differences.txt", "x")
 
-# with open('Seawall T3.cibd22x') as file_1, open('testing_output.cibd22x') as file_2:
-#     differ = Differ()
+with open(f1) as file_1, open(f2) as file_2:
+    differ = Differ()
 
-#     for line in differ.compare(file_1.readlines(), file_2.readlines()):
-#         wr_fname.write(line)
+    for line in differ.compare(file_1.readlines(), file_2.readlines()):
+        wr_fname.write(line)
 
-# wr_fname.close()
-# print("Differences Logged!")
+wr_fname.close()
+print("Differences Logged!")
