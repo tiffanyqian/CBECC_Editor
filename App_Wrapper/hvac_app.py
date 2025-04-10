015f4cc6914bee6cf2a1326cc19e673b09708f68
Tiffany Qian <tffnyqian@gmail.com>
2025-04-10 11:35:41 -0700

    Added interface to create Fluid Systems and Sub Components to hvac_app.py. Further inputs to be added.

--- App_Wrapper/hvac_app.py
+++ App_Wrapper/hvac_app.py
@@ -24,7 +24,8 @@ from PyQt6.QtWidgets import (
 
 class MainWindow(QMainWindow):
     def __init__(self):
-        global cbecc_file, ET_tree, ET_root, TZ_list, AS_list, ZS_list
+        global cbecc_file, ET_tree, ET_root, TZ_list, AS_list, ZS_list, FS_list
+        FS_list = []
         TZ_list = []
         AS_list = []
         ZS_list = []
@@ -62,10 +63,6 @@ class MainWindow(QMainWindow):
         self.curr_folder.setAlignment(Qt.AlignmentFlag.AlignHCenter | Qt.AlignmentFlag.AlignTop)
         folder_layout.addWidget(self.curr_folder,1,0)
 
-        # blank_lab = QLabel("")
-        # blank_lab.setFixedWidth(25)
-        # folder_layout.addWidget(blank_lab,0,1)
-
         lab_out = QLabel("Enter Output Filename:")
         lab_out.setFixedSize(250,25)
         folder_layout.addWidget(lab_out,0,2)
@@ -79,41 +76,55 @@ class MainWindow(QMainWindow):
 
         # Thermal Zone List Reference
         TZ_label = QLabel("ThermalZones on File:")
-        grid_layout.addWidget(TZ_label,1,0)
+        grid_layout.addWidget(TZ_label,0,0)
         self.TZ_widget = QListWidget()
         self.TZ_widget.addItems(TZ_list)
         self.TZ_widget.setSelectionMode(QAbstractItemView.SelectionMode.MultiSelection)
-        grid_layout.addWidget(self.TZ_widget,2,0,4,1)
+        grid_layout.addWidget(self.TZ_widget,1,0,2,1)
+
+        # Fluid System List Reference
+        FS_label = QLabel("FluidSystems on File:")
+        grid_layout.addWidget(FS_label,0,1)
+        self.FS_widget = QListWidget()
+        self.FS_widget.addItems(FS_list)
+        self.FS_widget.setSelectionMode(QAbstractItemView.SelectionMode.MultiSelection)
+        grid_layout.addWidget(self.FS_widget,1,1)
+        # Create / Edit FluidSys Button
+        self.create_fs_button = QPushButton("Create FluidSystem and Subcomponents\n(ChilledWater, CondenserWater, HotWater)\n(Chiller, Boiler, WaterHeater)")
+        grid_layout.addWidget(self.create_fs_button,2,1)
+        self.create_fs_button.setEnabled(False)
+        self.create_fs_button.clicked.connect(self.create_fs_button_clicked)
+
         # Air System List Reference
         AS_label = QLabel("AirSystems on File:")
-        grid_layout.addWidget(AS_label,1,1)
+        grid_layout.addWidget(AS_label,4,0)
         self.AS_widget = QListWidget()
         self.AS_widget.addItems(AS_list)
         self.AS_widget.setSelectionMode(QAbstractItemView.SelectionMode.MultiSelection)
-        grid_layout.addWidget(self.AS_widget,2,1)
+        grid_layout.addWidget(self.AS_widget,5,0)
         # Zone System List Reference
         ZS_label = QLabel("ZoneSystems on File:")
-        grid_layout.addWidget(ZS_label,1,2)
+        grid_layout.addWidget(ZS_label,4,1)
         self.ZS_widget = QListWidget()
         self.ZS_widget.addItems(ZS_list)
         self.ZS_widget.setSelectionMode(QAbstractItemView.SelectionMode.MultiSelection)
-        grid_layout.addWidget(self.ZS_widget,2,2)
+        grid_layout.addWidget(self.ZS_widget,5,1)
 
         # Create Multi Zone AirSys Button
         self.create_mz_as_button = QPushButton("Create Multi-Zone AirSystem\n(DOAS, VAV, PVAV)")
-        grid_layout.addWidget(self.create_mz_as_button,3,1)
+        grid_layout.addWidget(self.create_mz_as_button,6,0)
         self.create_mz_as_button.setEnabled(False)
         self.create_mz_as_button.clicked.connect(self.create_mz_as_button_clicked)
 
         # Create Single Zone AirSys Button
         self.create_sz_as_button = QPushButton("Create Single Zone AirSystem\n(SZHP, SZAC, SZVAVHP, SZVAVAC, Exhaust)")
-        grid_layout.addWidget(self.create_sz_as_button,4,1)
+        grid_layout.addWidget(self.create_sz_as_button,7,0)
         self.create_sz_as_button.setEnabled(False)
         self.create_sz_as_button.clicked.connect(self.create_sz_as_button_clicked)
 
         # Create Zone System Button
         self.create_sz_zs_button = QPushButton("Create ZoneSystem\n(SZHP, FPFC, VRF, Exhaust, etc.)")
-        grid_layout.addWidget(self.create_sz_zs_button,3,2)
+        grid_layout.addWidget(self.create_sz_zs_button,6,1)
         self.create_sz_zs_button.setEnabled(False)
         self.create_sz_zs_button.clicked.connect(self.create_sz_zs_button_clicked)
 
@@ -131,7 +142,7 @@ class MainWindow(QMainWindow):
         central_widget.setLayout(frame_layout)
 
     def open_file_clicked(self):
-        global cbecc_file, ET_tree, ET_root, TZ_list, AS_list, ZS_list
+        global cbecc_file, ET_tree, ET_root, TZ_list, AS_list, ZS_list, FS_list
         ET_root = 0
 
         # Clearing inputs / lists
@@ -142,21 +153,23 @@ class MainWindow(QMainWindow):
         self.create_sz_as_button.setEnabled(False)
         self.create_sz_zs_button.setEnabled(False)
         self.create_mz_as_button.setEnabled(False)
+        FS_list = []
         TZ_list = []
         AS_list = []
         ZS_list = []
 
         # Taking in the new CBECC file
         cbecc_file = ""
-        cbecc_file, end_discard = QFileDialog.getOpenFileName(self, "Select File",r"C://","2022 CBECC Files (*.cibd22x)")
+        cbecc_file, end_discard = QFileDialog.getOpenFileName(self, "Select File",r"C://","CBECC Files (*.cibd22x *.cibd22 *.cibd19x *cibd19)")
 
         # Loading stuff in
         if len(cbecc_file) != 0:
-            if re.search(".cibd22x",cbecc_file):
+            if re.search(".cibd",cbecc_file):
                 self.curr_folder.setText(cbecc_file)
                 self.f_output.setText(re.findall(r".*/(.*$)",cbecc_file)[0])
                 
                 self.f_output.setEnabled(True)
+                self.create_fs_button.setEnabled(True)
                 self.create_sz_as_button.setEnabled(True)
                 self.create_mz_as_button.setEnabled(True)
                 self.create_sz_zs_button.setEnabled(True)
@@ -173,18 +186,33 @@ class MainWindow(QMainWindow):
             self.update_lists()
 
     def update_lists(self):
-        global TZ_list, AS_list, ZS_list, ET_root
+        global FS_list, AS_list, ZS_list, ET_root
 
+        FS_list = []
         self.AS_widget.clear()
         self.ZS_widget.clear()
-        
+        self.FS_widget.clear()
+
+        for fldsys in ET_root.findall(".//FluidSys"):
+            FS_list.append(fldsys[0].text)
         for airsys in ET_root.findall(".//AirSys"):
             AS_list.append(airsys[0].text)
         for zs in ET_root.findall(".//ZnSys"):
             ZS_list.append(zs[0].text)
 
+        self.FS_widget.addItems(FS_list)
         self.AS_widget.addItems(AS_list)
         self.ZS_widget.addItems(ZS_list)
+
+    def create_fs_button_clicked(self):
+        dlg = FS_Dialog()
+        dlg.setWindowTitle("FluidSystem Creation")
+        if dlg.exec():
+            print("Completed FluidSystem Creation")
+            self.update_lists()
+            self.run_button.setEnabled(True)
+        else:
+            print("Canceled FluidSystem Creation")
 
     def create_sz_as_button_clicked(self):
         dlg = SZ_AS_Dialog()
@@ -223,6 +251,232 @@ class MainWindow(QMainWindow):
         
         hvac.save_changes(ET_tree, output_filename)
         print("File saved to:", output_filename)
+
+class FS_Dialog(QDialog):
+    global FS_list, ET_root, ET_tree
+    def __init__(self):
+        super().__init__()
+        
+        self.fs_list = FS_list
+
+        layout = QGridLayout()
+        
+        # FluidSystem Specifics / Creation
+        self.fs_tab = QTabWidget()
+        self.fs_tab_widg = QWidget()
+        fs_layout = QGridLayout()
+        self.fs_tab_widg.setLayout(fs_layout)
+
+        fs_layout.addWidget(QLabel("FluidSystem Type:"),0,0)
+        self.fs_type = QComboBox()
+        self.fs_type.addItems(["ChilledWater","CondenserWater","HotWater"])
+        fs_layout.addWidget(self.fs_type,0,1)
+        fs_layout.addWidget(QLabel("Control System Type"),1,0)
+        self.sys_ctrl = QComboBox()
+        self.sys_ctrl.addItems(["Other","DDC"])
+        fs_layout.addWidget(self.sys_ctrl,1,1)
+        fs_layout.addWidget(QLabel("Temperature Control"),2,0)
+        self.temp_ctrl = QComboBox()
+        self.temp_ctrl.addItems(["Fixed","Scheduled","OutsideAirReset","WetBulbReset","FixedDualSetpoint"])
+        fs_layout.addWidget(self.temp_ctrl,2,1)
+        
+        self.fs_tab.addTab(self.fs_tab_widg, "Create FluidSystem Specifics")
+        layout.addWidget(self.fs_tab,1,0,2,1)
+
+        self.fs_button = QPushButton("Create FluidSystem")
+        self.fs_button.setFixedHeight(50)
+        self.fs_button.clicked.connect(self.fs_button_clicked)
+        layout.addWidget(self.fs_button,3,0,2,1)
+
+        layout.addWidget(QLabel("Select FluidSystem to create\nChillers, Boilers, or WaterHeaters:"),0,2,1,1)
+        self.widget_fs = QListWidget()
+        self.widget_fs.clear()
+        self.widget_fs.addItems(self.fs_list)
+        self.widget_fs.itemSelectionChanged.connect(self.fs_selection_changed)
+        layout.addWidget(self.widget_fs,1,2,2,1)
+
+        layout.addWidget(QLabel("Subcomponents of Selected FluidSystem:"),3,2,1,1)
+        self.widget_fs_sc = QListWidget()
+        self.widget_fs_sc.addItems(["No Selection"])
+        self.widget_fs_sc.setSelectionMode(QAbstractItemView.SelectionMode.NoSelection)
+        layout.addWidget(self.widget_fs_sc,4,2,4,1)
+
+        layout.addWidget(QLabel("Subcomponent Type:"),0,3)
+        self.fs_sc_type = QComboBox()
+        self.fs_sc_type.addItems(["Chiller","Boiler","WaterHeater"])
+        layout.addWidget(self.fs_sc_type,0,4)
+        self.fs_sc_type.currentTextChanged.connect(self.type_changed)
+
+        layout.addWidget(QLabel("Number to Create:"),1,3)
+        self.fs_sc_num = QLineEdit("1")
+        self.fs_sc_num.setInputMask("d0")
+        layout.addWidget(self.fs_sc_num,1,4)
+
+        self.tab = QTabWidget()
+
+        # Chiller Specifics
+        self.ch_tab = QWidget()
+        ch_layout = QGridLayout()
+        self.ch_tab.setLayout(ch_layout)
+        ch_layout.addWidget(QLabel("Type:"),0,0)
+        self.ch_type = QComboBox()
+        self.ch_type.addItems(["Screw","Centrifugal","Reciprocating","Scroll","AbsorptionSingleEffect"])
+        ch_layout.addWidget(self.ch_type,0,1)
+        ch_layout.addWidget(QLabel("Condenser Type:"),1,0)
+        self.ch_cond_type = QComboBox()
+        self.ch_cond_type.addItems(["Air","Fluid"])
+        ch_layout.addWidget(self.ch_cond_type,1,1)
+        ch_layout.addWidget(QLabel("Pump:"),2,0)
+        self.ch_pump = QComboBox()
+        self.ch_pump.addItems(["VariableSpeed","ConstantSpeed"])
+        ch_layout.addWidget(self.ch_pump,2,1)
+
+        # Boiler Specifics
+        self.bl_tab = QWidget()
+        bl_layout = QGridLayout()
+        self.bl_tab.setLayout(bl_layout)
+        bl_layout.addWidget(QLabel("Type:"),0,0)
+        self.bl_type = QComboBox()
+        self.bl_type.addItems(["HotWater","Steam"])
+        bl_layout.addWidget(self.bl_type,0,1)
+        bl_layout.addWidget(QLabel("Fuel:"),1,0)
+        self.bl_fuel_type = QComboBox()
+        self.bl_fuel_type.addItems(["Gas","Electric"])
+        bl_layout.addWidget(self.bl_fuel_type,1,1)
+        bl_layout.addWidget(QLabel("Pump:"),2,0)
+        self.bl_pump = QComboBox()
+        self.bl_pump.addItems(["VariableSpeed","ConstantSpeed"])
+        bl_layout.addWidget(self.bl_pump,2,1)
+
+        # WaterHeater Specifics
+        self.wh_tab = QWidget()
+        wh_layout = QGridLayout()
+        self.wh_tab.setLayout(wh_layout)
+        wh_layout.addWidget(QLabel("Type:"),0,0)
+        self.wh_type = QComboBox()
+        self.wh_type.addItems(["HeatPumpSplit","HeatPumpPackaged"])
+        wh_layout.addWidget(self.wh_type,0,1)
+        wh_layout.addWidget(QLabel("Condenser Type:"),1,0)
+        self.wh_subtype = QComboBox()
+        self.wh_subtype.addItems(["R134a_MultiPass","R410a_MultiPass"])
+        wh_layout.addWidget(self.wh_subtype,1,1)
+        wh_layout.addWidget(QLabel("Pump:"),2,0)
+        self.wh_pump = QComboBox()
+        self.wh_pump.addItems(["VariableSpeed","ConstantSpeed"])
+        wh_layout.addWidget(self.wh_pump,2,1)
+
+        self.tab.addTab(self.ch_tab, "Edit Chiller Specifics")
+        self.tab.addTab(self.bl_tab, "Edit Boiler Specifics")
+        self.tab.addTab(self.wh_tab, "Edit WaterHeater Specifics")
+        self.tab.setTabVisible(0,True)
+        self.tab.setTabVisible(1,False)
+        self.tab.setTabVisible(2,False)
+        layout.addWidget(self.tab,2,3,1,2)
+
+        self.fs_sc_button = QPushButton("Create Chiller(s)")
+        self.fs_sc_button.setFixedHeight(50)
+        self.fs_sc_button.clicked.connect(self.fs_sc_button_clicked)
+        # self.fs_sc_button.clicked.connect(self.type_changed)
+        self.fs_sc_button.setEnabled(False)
+        layout.addWidget(self.fs_sc_button,3,3,2,2)
+
+        QBtn = (
+            QDialogButtonBox.StandardButton.Ok | QDialogButtonBox.StandardButton.Cancel
+        )
+        self.buttonBox = QDialogButtonBox(QBtn)
+        self.buttonBox.clicked.connect(self.button_clicked)
+        layout.addWidget(self.buttonBox,7,4)
+
+        self.setLayout(layout)
+
+    def type_changed(self, s):
+        # print(s)
+        if s == "Chiller":
+            self.tab.setCurrentWidget(self.ch_tab)
+            self.tab.setTabVisible(0,True)
+            self.tab.setTabVisible(1,False)
+            self.tab.setTabVisible(2,False)
+            self.fs_sc_button.setText("Create Chiller(s)")
+        elif s == "Boiler":
+            self.tab.setCurrentWidget(self.bl_tab)
+            self.tab.setTabVisible(0,False)
+            self.tab.setTabVisible(1,True)
+            self.tab.setTabVisible(2,False)
+            self.fs_sc_button.setText("Create Boiler(s)")
+        elif s == "WaterHeater":
+            self.tab.setCurrentWidget(self.wh_tab)
+            self.tab.setTabVisible(0,False)
+            self.tab.setTabVisible(1,False)
+            self.tab.setTabVisible(2,True)
+            self.fs_sc_button.setText("Create WaterHeater(s)")
+
+    def fs_selection_changed(self):
+        if len(self.widget_fs.selectedItems()) > 0:
+            self.fs_sc_button.setEnabled(True)
+            
+            fs_select_name = self.widget_fs.selectedItems()[0].text()
+            
+            self.widget_fs_sc.clear()
+            self.fs_sc_list = []
+            for fs in ET_root.findall(".//FluidSys"):
+                if fs[0].text == fs_select_name:
+                    for ch in fs.findall(".//Chlr"):
+                        self.fs_sc_list.append(ch[0].text)
+                    for bl in fs.findall(".//Blr"):
+                        self.fs_sc_list.append(bl[0].text)
+                    for wh in fs.findall(".//WtrHtr"):
+                        self.fs_sc_list.append(wh[0].text)
+            self.widget_fs_sc.addItems(self.fs_sc_list)
+        else:
+            self.fs_sc_button.setEnabled(False)
+
+    def fs_button_clicked(self, button):
+        global ET_root, ET_tree
+
+        proj = ET_root.findall(".//Proj")[0]
+        fs_type = self.fs_type.currentText()
+
+        # Run FluidSys creator method and add to list
+        added_fs = hvac.FluidSys(proj, fs_type, self.sys_ctrl.currentText(), self.temp_ctrl.currentText())
+        self.widget_fs.addItem(added_fs)
+
+        self.buttonBox.button(QDialogButtonBox.StandardButton.Ok).setEnabled(True)
+    
+    def fs_sc_button_clicked(self, button):
+        global ET_root, ET_tree
+
+        fs_name = self.widget_fs.selectedItems()[0].text()
+        sc_type = self.fs_sc_type.currentText()
+        sc_num = int(self.fs_sc_num.text())
+        if sc_num < 0:
+            sc_num = 1
+        
+        fluidsys = 0
+        for fs in ET_root.findall(".//FluidSys"):
+            if fs[0].text == fs_name:
+                fluidsys = fs
+
+        if fluidsys != 0:
+            if sc_type == "Chiller":
+                hvac.FS_Chiller(fluidsys, create_num=sc_num, ch_type=self.ch_type.currentText(), cond_type=self.ch_cond_type.currentText(), pump_spd_ctrl=self.ch_pump.currentText())
+            elif sc_type == "Boiler":
+                hvac.FS_Boiler(fluidsys, create_num=sc_num, bl_type=self.bl_type.currentText(), fuel_src=self.bl_fuel_type.currentText(), pump_spd_ctrl=self.bl_pump.currentText())
+            elif sc_type == "WaterHeater":
+                hvac.FS_WaterHeater(fluidsys, create_num=sc_num, wh_type=self.wh_type.currentText(), wh_subtype=self.wh_subtype.currentText(), pump_spd_ctrl=self.wh_pump.currentText())
+            
+            self.fs_selection_changed()
+        else:
+            print("FluidSystem not found, something went wrong.")
+        
+        self.buttonBox.button(QDialogButtonBox.StandardButton.Ok).setEnabled(True)
+
+    def button_clicked(self, button):
+        role = self.buttonBox.standardButton(button)
+
+        if role == QDialogButtonBox.StandardButton.Cancel:
+            self.buttonBox.rejected.connect(self.reject)
+        elif role == QDialogButtonBox.StandardButton.Ok:
+            self.buttonBox.accepted.connect(self.accept)
 
 class SZ_AS_Dialog(QDialog):
     global TZ_list, ET_root
